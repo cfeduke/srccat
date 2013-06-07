2c0f70cae0ffac7851b3ba13435efc17e8beafc3
Charles Feduke <charles.feduke@gmail.com>
2013-06-07 11:19:45 -0400

    roughed in srccat

---
+++ srccat/srccat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+# composes a colout command for syntax highlighting source code
+# while colout is capable of doing this itself it requires hints
+# for determining which language to use when syntax highlighting
+# through pygments
+# Licensed under GPL v3
+# (c)2013 Charles Feduke <charles.feduke@gmail.com>
+
+import sys
+import os
+
+
+if __name__ == '__main__':
+    pass
