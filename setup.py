1758b6ff0614b607ed84c422818e864bfe898774
Charles Feduke <charles.feduke@gmail.com>
2013-06-07 11:08:50 -0400

    copied and modified setup.py from colout, added GNU GPL license

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import srccat
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = ['srccat']
+
+requires = ['colout']
+
+setup(
+    name='srccat',
+    version='0.1',
+    description='Syntax highlights source code to terminal using colout',
+    long_description=open('README.md').read(),
+    author='cfeduke',
+    author_email='charles.feduke@gmail.com',
+    url='http://cfeduke.github.com/srccat/',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'srccat': 'srccat'},
+    scripts=['bin/srccat'],
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+)
