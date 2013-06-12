9a4be6491f3d0ac58b71826f52584b3e2446cf59
Charles Feduke <charles.feduke@gmail.com>
2013-06-12 15:59:08 -0400

    filename is extension

--- tests/test_language_association.py
+++ tests/test_language_association.py
@@ -14,5 +14,8 @@ class TestLanguageAssociation(TestCase):
     def test__call__when_extension_does_not_exist_returns_text_only(self):
         assert self.subject("file.nonexistent-ext") == DEFAULT_LANGUAGE
 
+    def test__call__when_filename_is_extension(self):
+        assert self.subject("gvimrc") == "VimL"
+
 
 
