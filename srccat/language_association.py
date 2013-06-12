4e756baa31b4b87812510813e8131324fb884c5e
Charles Feduke <charles.feduke@gmail.com>
2013-06-12 15:50:42 -0400

    passing tests

--- srccat/language_association.py
+++ srccat/language_association.py
@@ -1,8 +1,13 @@
+from extension_language_index import EXTENSION_LANGUAGE_INDEX
 
+DEFAULT_LANGUAGE = "Text only"
 
 class LanguageAssociation:
     def __init__(self):
         pass
 
     def __call__(self, ext):
-        return "Python"
+        if ext not in EXTENSION_LANGUAGE_INDEX.keys():
+            return DEFAULT_LANGUAGE
+
+        return EXTENSION_LANGUAGE_INDEX[ext]
--- tests/test_language_association.py
+++ tests/test_language_association.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from srccat.language_association import LanguageAssociation
+from srccat.language_association import LanguageAssociation, DEFAULT_LANGUAGE
 
 
 class TestLanguageAssociation(TestCase):
@@ -10,6 +10,9 @@ class TestLanguageAssociation(TestCase):
 
     def test__call__when_extension_exists(self):
         assert self.subject(".py") == "Python"
+
+    def test__call__when_extension_does_not_exist_returns_text_only(self):
+        assert self.subject(".nonexistent-ext") == DEFAULT_LANGUAGE
 
 
 
